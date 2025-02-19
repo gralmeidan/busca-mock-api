d52734d2007747525d594605faa2d96367d371b4
gralmeidan <gralmeidan@gmail.com>
2025-02-19 12:05:45 -0400

    chore: add documentation and validation packages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, new DocumentBuilder().build())
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -3,9 +3,10 @@ import { AirportModule } from 'src/airport';
 import { SearchService } from './search.service';
 import { SearchMock } from './search.mock';
 import { SearchController } from './search.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [AirportModule],
+  imports: [AirportModule, CacheModule.register()],
   providers: [SearchService, SearchMock],
   controllers: [SearchController],
 })
